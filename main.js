fe6014f72b8ea2e5b07ca9136a0fe6fe81affa7f
tt <tiantian7@xiaomi.com>
2021-01-30 16:00:30 +0800

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+for (let i of [1,2,3,4,5]) {
+    console.log(i)
+}
+
+
+function createElement(tagName, attributes, ...children) {
+    
+    return document.createElement(tagName);
+
+}
+
+window.a = <div id="a" class="c">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
