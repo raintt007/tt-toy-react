da9c6682652d0f2cbb40397d001c4c7265caf5b9
tt <695768421@qq.com>
2021-02-19 00:23:13 +0800

    feat vdom

--- toy-react.js
+++ toy-react.js
@@ -1,6 +1,54 @@
 let RENDER_TO_DOM = Symbol('render to dom');
-class ElementWrapper {
+
+export class Component {
+    constructor() {
+        this.props = Object.create(null)
+        this.children = [];
+        this._root = null;
+    }
+    setAttribute(name, value) {
+        this.props[name] = value;
+    } 
+    appendChild(component) {
+        this.children.push(component);
+    }
+    // rangeAPI
+    [RENDER_TO_DOM](range) {
+        this.render()[RENDER_TO_DOM](range);
+        this._vdom = this._vdom;
+        this._vdom[RENDER_TO_DOM](range);
+    }
+
+    update() {
+        let isSameNode = (oldNode, newNode) => {
+            if (oldNode.type !== newNode.type) {
+                return false;
+            }
+            for (let name in newNode.props) {
+                if (newNode.props[name] !== oldNode.props[name]) {
+                    return false;
+                }
+            }
+            if (Object.keys(oldNode.props).length > Object.keys(newNode.props).length) {
+                return false;
+            }
+            if (newNode.type === "#text") {
+                if (newNode.content !== oldNode.content) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        let update = （oldNode, newNode) => {
+
+        }
+    }
+}
+
+class ElementWrapper extends Component{
     constructor(type) {
+        super();
+        this.type = type
         this.root = document.createElement(type);
     }
 
@@ -10,12 +58,20 @@ class ElementWrapper {
     appendChild(component) {
         this.root.appendChild(component.root);
     }
-
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
+    }
 }
 
-class TextWrapper {
+class TextWrapper extends component{
     constructor(content) {
+        super(content);
         this.root = document.createTextNode(content);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -68,11 +124,13 @@ export function createElement(type, attributes, ...children) {
         }
     }
     insertChildren(children);
-    
     return e;
-
 }
 
 export function render(component, parentElement) {
-    parentElement.appendChild(component.root)
+    let range = document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parmentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
